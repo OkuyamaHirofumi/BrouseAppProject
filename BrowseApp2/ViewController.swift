8262ad769e844eb932fac5881d2d26f158a2b61f
OkuyamaHirofumi <torukodevelop@gmail.com>
2015-03-02 22:38:25 +0900

    Initial Commit

---
+++ BrowseApp2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BrowseApp2
+//
+//  Created by 奥山博史 on 2015/03/02.
+//  Copyright (c) 2015年 奥山博史. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
